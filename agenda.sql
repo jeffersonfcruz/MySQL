1679cdf8c3878686db8649ce5f263af907af9414
Jefferson Cruz <jeffcruz1998@hotmail.com>
2022-05-16 16:38:58 -0300

    inicio do projeto bando de dados

---
+++ agenda.sql
@@ -0,0 +1,13 @@
+/**
+	Agenda de Contatos
+    @author Jefferson Cruz
+*/
+-- Verificar banco de dados 
+show databases;
+-- Criar um novo banco da dados
+create database teste;
+create database agendajefferson;
+-- Excluir um banco de dados
+drop database teste;
+-- Selecionar um banco de dados
+use agendajefferson;
